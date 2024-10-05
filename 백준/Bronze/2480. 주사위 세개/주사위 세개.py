adb6786eb8614506a7e5cec695b6c61c1b4d5501
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2024-10-05 12:49:51 +0900

    Update 주사위 세개.py

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,26 +1,14 @@
-dice = input()
-
-dice = dice.split()
-
-dice[0] = int(dice[0])
-dice[1] = int(dice[1])
-dice[2] = int(dice[2])
-
+dice = list(map(int, input().split()))
 
 money = 0
 
 
-if dice[0] == dice[1] and dice[1] == dice[2]:
+if len(set(dice)) == 1:
     money += 10000 + dice[0] * 1000
     
-elif dice[0] != dice[1] and dice[1] != dice[2] and dice[0] != dice[2]:
-    money += 100 * max(dice)
+elif len(set(dice)) == 2:
+    money += 1000 + (sum(dice) - min(dice) - max(dice)) * 100
 else:
-    if dice[0] == dice[1]:
-        money += 1000 + dice[0] * 100
-    elif dice[1] == dice[2]:
-        money += 1000 + dice[1] * 100
-    else:
-        money += 1000 + dice[2] * 100
+    money += max(dice) * 100
         
 print(money)
