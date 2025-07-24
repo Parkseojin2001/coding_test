8e33c90e7568cd201742190058a698c4db53fa25
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-07-24 21:34:00 +0900

    [level 1] Title: 삼총사, Time: 0.09 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,10 +1,10 @@
+import itertools
+
 def solution(number):
-    number.sort()
-    
     count = 0
-    for left in range(0, len(number) - 2):
-        for mid in range(left + 1, len(number) - 1):
-            for right in range(mid + 1, len(number)):
-                if number[left] + number[mid] + number[right] == 0:
-                    count += 1
+    comb = itertools.combinations(number, 3)
+    for c in comb:
+        if sum(c) == 0:
+            count += 1
     return count
+    
