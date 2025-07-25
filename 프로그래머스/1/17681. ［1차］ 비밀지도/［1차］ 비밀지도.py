71677aa837567c5a00538858b3ca3f443be43482
Parkseojin2001 <85439023+Parkseojin2001@users.noreply.github.com>
2025-07-26 01:07:40 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,37 +1,13 @@
 def solution(n, arr1, arr2):
-    def change_bit(num, k):
-        two_bit = []
-        while num > 0:
-            two_bit.append('#' if num % 2 == 1 else ' ')
-            num //= 2
-        if len(two_bit) < k:
-            for i in range(k - len(two_bit)):
-                two_bit.append(' ')
-        return two_bit
-    
-    matrix_1 = []
-    matrix_2 = []
-    
-    for i in arr1:
-        r = change_bit(i, n)
-        r.reverse()
-        matrix_1.append(r)
-    
-    for i in arr2:
-        r = change_bit(i, n)
-        r.reverse()
-        matrix_2.append(r)
-        
-    for i in range(len(matrix_1)):
-        for j in range(len(matrix_1[i])):
-            if matrix_2[i][j] == '#':
-                matrix_1[i][j] = '#'
-    
     answer = []
-    print(matrix_1)
-    for a in matrix_1:
-        s = ''.join(a)
-        answer.append(s)
     
+    for i, j in zip(arr1, arr2):
+        a12 = str(bin(i|j)[2:])
+        a12 = a12.rjust(n, '0')
+        a12 = a12.replace('1', '#')
+        a12 = a12.replace('0', ' ')
+        
+        answer.append(a12)
+        
     return answer
             
