a584822dd0dfb400589256aeb7096ec8c8b48255
Seojin Park <85439023+Parkseojin2001@users.noreply.github.com>
2025-08-07 23:55:34 +0900

    [Silver V] Title: 스네이크버드, Time: 36 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+
+input = sys.stdin.readline
+
+N, L = map(int, input().split())
+
+heights = list(map(int, input().split()))
+
+heights.sort()
+
+for h in heights:
+    if h <= L:
+        L += 1
+    else:
+        break
+print(L)
