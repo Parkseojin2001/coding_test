a6b67cf532efcf5901a9a943395dcf664483debe
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-03-04 22:34:16 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def stars(n: int, m: int) -> str:
+    return "*" * (m - n)
+
+
+def main() -> None:
+    N = int(sys_input())
+    for i in range(0, N):
+        print(stars(i, N))
+
+
+if __name__ == "__main__":
+    main()
