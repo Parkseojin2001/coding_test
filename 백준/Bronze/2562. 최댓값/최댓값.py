9d15d98e9f1d904c81446d1e3a1adc8d5ed0ba1f
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-03-05 01:51:03 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def solve(nums: list[int]) -> tuple[int, int]:
+    max_val = max(nums)
+
+    max_idx = nums.index(max_val) + 1
+    return max_val, max_idx
+
+
+def main() -> None:
+    nums = [int(sys_input()) for _ in range(9)]
+    answer: tuple[int, int] = solve(nums)
+    print(answer[0])
+    print(answer[1])
+
+
+if __name__ == "__main__":
+    main()
