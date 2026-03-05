9947d1d005f11146e4c44c4f99f472e86c2c302d
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-03-05 23:55:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -6,7 +6,7 @@ def sys_input() -> str:
     return sys.stdin.readline().rstrip()
 
 
-def solve(n: int, k: int) -> list[str]:
+def solve(n: int, k: int) -> str:
     nums = []
     circle = deque(range(1, n + 1))
 
@@ -14,14 +14,14 @@ def solve(n: int, k: int) -> list[str]:
         circle.rotate(-k)
         nums.append(str(circle.pop()))
 
-    return nums
+    return "<" + ", ".join(nums) + ">"
 
 
 def main() -> None:
     N, K = map(int, sys_input().split())
 
-    answer: list[str] = solve(N, K)
-    print(f"<{', '.join(answer)}>")
+    answer: str = solve(N, K)
+    print(answer)
 
 
 if __name__ == "__main__":
