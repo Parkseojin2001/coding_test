7e6d5a744e34054e091ca06ffeb59d3b8e3647fd
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-03-12 17:26:42 +0900

    [Gold V] Title: 적록색약, Time: 64 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,56 +1,61 @@
 import sys
-from collections import defaultdict
 from collections import deque
-import heapq
 
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+def fill_flood(board: list[str], is_blind=False) -> int:
+    n = len(board)
+    m = len(board[0])
+
+    visited = [[False] * m for _ in range(n)]
+    cnt = 0
+
+    def bfs(start: tuple[int, int], colors: set[str]) -> None:
+        deq = deque([start])
+        visited[start[0]][start[1]] = True
+
+        while deq:
+            x, y = deq.popleft()
+
+            for dx, dy in DIRECTIONS:
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < n and 0 <= ny < m and board[nx][ny] in colors and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    deq.append((nx, ny))
+
+    for x in range(n):
+        for y in range(m):
+            if not visited[x][y]:
+                curr_color = board[x][y]
+                if is_blind and curr_color in 'RG':
+                    bfs((x, y), {'R', "G"})
+                else:
+                    bfs((x, y), {curr_color})
+                cnt += 1
+    return cnt
 
 
+def solve(board: list[str]) -> tuple[int, int]:
+    not_blind = fill_flood(board)
+    is_blind = fill_flood(board, is_blind=True)
 
-N = int(input())
+    return not_blind, is_blind
 
-painting = [input().strip() for _ in range(N)]
-painting_1 = []
-painting_2 = []
-
-for i in range(N):
-    painting_1.append(list(painting[i]))
-    painting_2.append(list(painting[i].replace('G', 'R')))
-
-
-normal = [0] * 3
-unnormal = [0] * 2
-
-def dfs(paint, x, y, color):
-    if x >= N or x <= -1 or y >= N or y <= -1:
-        return False
     
-    if paint[x][y] == color:
-        paint[x][y] = 0
-        dfs(paint, x + 1, y, color)
-        dfs(paint, x - 1, y, color)
-        dfs(paint, x, y - 1, color)
-        dfs(paint, x, y + 1, color)
-        return True
-    
-    return False
+
+def main() -> None:
+    N = int(sys_input())
+    board = [sys_input() for _ in range(N)]
+
+    answer: tuple[int, int] = solve(board)
+
+    print(answer[0], answer[1])
 
 
-for i in range(N):
-    for j in range(N):
-        if dfs(painting_1, i, j, 'R'):
-            normal[0] += 1
-        if dfs(painting_1, i, j, 'G'):
-            normal[1] += 1
-        if dfs(painting_1, i, j, 'B'):
-            normal[2] += 1
-            
-for i in range(N):
-    for j in range(N):
-        if dfs(painting_2, i, j, 'R'):
-            unnormal[0] += 1
-        if dfs(painting_2, i, j, 'B'):
-            unnormal[1] += 1
-            
-print(sum(normal), sum(unnormal))
+
+if __name__ == "__main__":
+    main()
