874de8b2f69d11d348e48c8460aecdc0829b323c
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-03-12 19:58:27 +0900

    [Silver I] Title: 나이트의 이동, Time: 1804 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -5,7 +5,7 @@ from collections import deque
 DIRECTIONS = [(1, 2), (2, 1), (-1, 2), (-2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
 
 
-def sys_input() -> None:
+def sys_input() -> str:
     return sys.stdin.readline().rstrip()
 
 def bfs(l: int, start: tuple[int, int], dest: tuple[int, int]) -> None:
