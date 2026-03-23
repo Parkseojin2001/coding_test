d3b8583f60cc7df9c275492f76156d38ba660c8f
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-03-24 01:29:27 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3664 ms, Memory: 184204 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,48 @@
+import sys
+from collections import deque
+
+DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+MAX_BROKEN = 1
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+def bfs(n: int, m: int, maps: list[str]) -> int:
+    queue = deque([(0, 0, 0)])
+    dist = [[[-1] * 2 for _ in range(m)] for _ in range(n)]
+    dist[0][0][0] = 1
+
+    while queue:
+        x, y, broken_cnt = queue.popleft()
+        if (x, y) == (n - 1, m - 1):
+            return dist[x][y][broken_cnt]
+        for dx, dy in DIRECTIONS:
+            nx, ny = x + dx, y + dy
+            next_dist = dist[x][y][broken_cnt] + 1
+            if not (0 <= nx < n and 0 <= ny < m):
+                continue
+            if maps[nx][ny] == "0" and dist[nx][ny][broken_cnt] == -1:
+                dist[nx][ny][broken_cnt] = next_dist
+                queue.append((nx, ny, broken_cnt))
+            if maps[nx][ny] == "1" and broken_cnt < MAX_BROKEN and dist[nx][ny][broken_cnt + 1] == -1:
+                dist[nx][ny][broken_cnt + 1] = next_dist
+                queue.append((nx, ny, broken_cnt + 1))
+
+    return -1
+
+
+def solve(n: int, m: int, maps: list[str]) -> int:
+    return bfs(n, m, maps)
+
+
+
+def main() -> None:
+    N, M = map(int, sys_input().split())
+    maps = [sys_input().rstrip() for _ in range(N)]
+
+    answer: int = solve(N, M, maps)
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
