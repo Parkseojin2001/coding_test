8859ec8fba2cc83b372f66558946eb2046b220d0
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-04-03 00:32:22 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 924 ms, Memory: 90836 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -2,34 +2,29 @@ import sys
 
 POLES = {1, 2, 3}
 
-
 def sys_input() -> str:
     return sys.stdin.readline().rstrip()
 
-
-def save_path(src: int, dest: int, n: int, path: list[str]) -> None:
+def solve(src: int, dest: int, n: int, path: list[str]) -> None:
     if n == 0:
         return
-
-    before = (POLES - {src, dest}).pop()
-    save_path(src, before, n - 1, path)
+    rest = (POLES - {src, dest}).pop()
+    solve(src, rest, n - 1, path)
     path.append(f"{src} {dest}")
-    save_path(before, dest, n - 1, path)
+    solve(rest, dest, n - 1, path)
 
-
-def hanoi(n: int) -> tuple[int, list[str]]:
+def hanoi(n: int) -> list[str]:
     path = []
-    save_path(1, 3, n, path)
-    return len(path), path
-
+    solve(1, 3, n, path)
+    return path
 
 def main() -> None:
     N = int(sys_input())
+    answer: list[str] = hanoi(N)
 
-    answer: tuple[int, list[str]] = hanoi(N)
-    print(answer[0])
-    for p in answer[1]:
-        print(p)
+    print(len(answer))
+    for k in answer:
+        print(k)
 
 
 if __name__ == "__main__":
