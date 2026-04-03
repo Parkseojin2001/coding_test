d574e6ae8aad344d4a57a62866a25d544b87dcfc
박서진_T8076 <85439023+Parkseojin2001@users.noreply.github.com>
2026-04-03 17:46:13 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -4,25 +4,11 @@ def sys_input() -> str:
     return sys.stdin.readline().rstrip()
 
 def solve(n: int, r: int, c: int) -> int:
-    if n == 1:
-        if r == 0 and c == 0:
-            return 0
-        elif r == 0 and c == 1:
-            return 1
-        elif r == 1 and c == 0:
-            return 2
-        else:
-            return 3
+    if n == 0:
+        return 0
     r_region = r // pow(2, n - 1)
     c_region = c // pow(2, n - 1)
-    if r_region == 0 and c_region == 0:
-        region = 0
-    elif r_region == 0 and c_region == 1:
-        region = 1
-    elif r_region == 1 and c_region == 0:
-        region = 2
-    else:
-        region = 3
+    region = 2 * r_region + c_region
     r = r % pow(2, n - 1)
     c = c % pow(2, n - 1)
     return solve(n - 1, r, c) + region * (pow(2, n - 1) * pow(2, n - 1))
